6deee5c32e6a495b89229c5b2f4c87b2269591de
islamosman2541998 <islamosman2541998@gmail.com>
2026-05-23 17:16:41 +0300

    Update head.blade.php

--- resources/views/components/site/layouts/head.blade.php
+++ resources/views/components/site/layouts/head.blade.php
@@ -64,7 +64,7 @@
     <link rel="stylesheet" href="{{ asset('site/css/animate.css') }}" />
     <link rel="stylesheet" href="{{ asset('site/css/swiper.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('site/css/swiper-bundle.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('site/css/style.css?v=0.0.11') }}" />
+    <link rel="stylesheet" href="{{ asset('site/css/style.css?v=0.0.12') }}" />
     <link rel="stylesheet" href="{{ asset('site/css/custom.css?v=0.0.11') }}" />
 
 
