2833237df8db2cb4939db177ae31cdeab678a99e
islamosman2541998 <islamosman2541998@gmail.com>
2026-06-11 16:44:04 +0300

    Update slider.blade.php

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -1,22 +1,20 @@
 @php
-    $settings     = \App\Settings\SettingSingleton::getInstance();
-    $show_slider    = (int) $settings->getHome('show_slider');
-    
+    $settings = \App\Settings\SettingSingleton::getInstance();
+    $show_slider = (int) $settings->getHome('show_slider');
+
 @endphp
 
-   <!-- Hero Section Begin -->
-    <section class="hero" dir="{{ app()->getLocale() == 'ar' ? 'rtl' : 'ltr' }}">
-        <div class="hero__slider owl-carousel">
+<!-- Hero Section Begin -->
+<section class="hero" dir="{{ app()->getLocale() == 'ar' ? 'rtl' : 'ltr' }}">
+    <div class="hero__slider owl-carousel">
 
-            @forelse ($slides as $slide)
-            <div class="hero__item set-bg bgImg"
-            {{-- style="background-image: url('{{ asset($slide->pathInView()) }}') !important;" --}}
-             data-setbg="{{ asset($slide->pathInView()) }}">
+        @forelse ($slides as $slide)
+            <div class="hero__item set-bg bgImg" {{-- style="background-image: url('{{ asset($slide->pathInView()) }}') !important;" --}} data-setbg="{{ asset($slide->pathInView()) }}">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="hero__text">
-                                <span>{!!  $slide->description !!} </span>
+                                <span class="slider-desc">{!! $slide->description !!} </span>
                                 <h2>{{ $slide->title }}</h2>
                                 <a href="#" class="primary-btn">See more about us</a>
                             </div>
@@ -24,15 +22,16 @@
                     </div>
                 </div>
             </div>
-            @empty
-            
+        @empty
+        @endforelse
 
 
-            @endforelse 
-          
-          
-        </div>
-    </section>
-    <!-- Hero Section End -->
+    </div>
+</section>
+<!-- Hero Section End -->
 
-  
+<style>
+    .slider-desc p {
+        color: #fff !important;
+    }
+</style>
