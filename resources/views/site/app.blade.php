7b34f993de0ee02364d7f70484971bec30faefdb
islamosman2541998 <islamosman2541998@gmail.com>
2026-07-20 12:50:32 +0300

    update

--- resources/views/site/app.blade.php
+++ resources/views/site/app.blade.php
@@ -27,6 +27,11 @@
 
     {!! \App\Settings\SettingSingleton::getInstance()->getScript('footer_script') !!}
 
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THXZWWKN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 </body>
 
 </html>
--- resources/views/site/pages/blogs/index.blade.php
+++ resources/views/site/pages/blogs/index.blade.php
@@ -1,79 +1,114 @@
 @extends('site.app')
 
 @section('title', @$metaSetting->where('key', 'blogs_meta_title_' . $current_lang)->first()->value)
+
 @section('meta_key', @$metaSetting->where('key', 'blogs_meta_key_' . $current_lang)->first()->value)
+
 @section('meta_description', @$metaSetting->where('key', 'blogs_meta_description_' . $current_lang)->first()->value)
 
 
 @section('content')
 
-
     <!-- Breadcrumb Begin -->
-    <div class="breadcrumb-option spad w-100 h-50 set-bg" data-setbg="{{ asset('site/img/111.jpeg') }}">
+    <div class="breadcrumb-option set-bg"
+        data-setbg="{{ asset('site/img/111.jpeg') }}">
+
         <div class="container">
+
             <div class="row">
+
                 <div class="col-lg-12 text-center">
+
                     <div class="breadcrumb__text">
-                        <h2> @lang('blogs.blogs')</h2>
+
+                        <h2>
+                            @lang('blogs.blogs')
+                        </h2>
+
                         <div class="breadcrumb__links">
-                            <a href="{{ route('site.home') }}">@lang('site.home') /</a>
-                            <span>@lang('blogs.blogs')</span>
+
+                            <a href="{{ route('site.home') }}">
+                                @lang('site.home') /
+                            </a>
+
+                            <span>
+                                @lang('blogs.blogs')
+                            </span>
+
                         </div>
+
                     </div>
+
                 </div>
+
             </div>
+
         </div>
+
     </div>
     <!-- Breadcrumb End -->
 
+
     <!-- Blog Section Begin -->
     <section class="blog spad">
+
         <div class="container">
+
             <div class="row">
 
-                @forelse ($blogs as $key =>$blog)
+                @forelse ($blogs as $key => $blog)
+
                     <div class="col-lg-4 col-md-6 col-sm-6 mb-3">
+
                         <div class="blog__item latest__item">
 
-                            <img src="{{ asset($blog->pathInView()) }}" alt="{{ $blog->title }}">
+                            <img src="{{ asset($blog->pathInView()) }}"
+                                alt="{{ $blog->title }}">
 
-                            <div class="blog-content d-flex flex-column justify-content-center align-items-center  p-3">
-                                <h4>{{ $blog->title }} </h4>
-                              
+                            <div
+                                class="blog-content d-flex flex-column justify-content-center align-items-center p-3">
+
+                                <h4>
+                                    {{ $blog->title }}
+                                </h4>
+
                                 <p>
                                     {!! Str::limit($blog->description, 65) !!}
-                                </p>  <ul>
-                                    <li>{{ $blog->created_at->format('M d, Y') }}</li>
+                                </p>
+
+                                <ul>
+                                    <li>
+                                        {{ $blog->created_at->format('M d, Y') }}
+                                    </li>
                                 </ul>
-                                <a href="{{ route('site.site.blogs.show', $blog->id) }}">@lang('admin.read_more') </a>
+
+                                <a href="{{ route('site.site.blogs.show', $blog->id) }}">
+                                    @lang('admin.read_more')
+                                </a>
+
                             </div>
+
                         </div>
+
                     </div>
+
                 @empty
-                    <h3>@lang('blogs.no_blogs')</h3>
+
+                    <div class="col-12 text-center">
+
+                        <h3 class="text-white">
+                            @lang('blogs.no_blogs')
+                        </h3>
+
+                    </div>
+
                 @endforelse
 
             </div>
-            {{-- <div class="row">
-                <div class="col-lg-12">
-                    <div class="pagination__option blog__pagi">
-                        <a href="#" class="arrow__pagination left__arrow"><span class="arrow_left"></span> Prev</a>
-                        <a href="#" class="number__pagination">1</a>
-                        <a href="#" class="number__pagination">2</a>
-                        <a href="#" class="arrow__pagination right__arrow">Next <span class="arrow_right"></span></a>
-                    </div>
-                </div>
-            </div> --}}
+
         </div>
+
     </section>
     <!-- Blog Section End -->
 
-
-
-
 @endsection
-<style>
-     i {
-        margin-top: 13px !important;
-    }
-</style>
